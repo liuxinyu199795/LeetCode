72a58e54c0a4ad099f28ef6098b932510ff31f98
liuxinyu199795 <38857686+liuxinyu199795@users.noreply.github.com>
2019-04-17 10:03:09 +0800

    Add files via upload

---
+++ LeetCode33.java
@@ -0,0 +1,35 @@
+package GaoPinSuanFa;
+
+
+import java.util.Stack;
+
+public class LeetCode33 {
+    public static int search(int[] nums, int target) {
+        return search(nums,0,nums.length-1,target);
+    }
+    public static int search(int[] nums,int low,int high,int target){
+        if(low>high) return -1;
+        int mid=(low+high)/2;
+        if(nums[mid]==target) return mid;
+        if(nums[mid]<nums[high]){
+            if(nums[mid]<target&&target<=nums[high]){
+                return search(nums,mid+1,high,target);
+            }else{
+                return search(nums,low,mid-1,target);
+            }
+        }else{
+            if(nums[low]<=target&&target<nums[mid]){
+                return search(nums,low,mid-1,target);
+            }else{
+                return search(nums,mid+1,high,target);
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        int[] nums={4,5,6,7,0,1,2};
+        System.out.println(search(nums,2));
+        System.out.println(search(nums,3));
+        System.out.println(search(nums,0));
+    }
+}
