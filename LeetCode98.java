084507735fdb661b87f61127276f6012cc2bb0ee
liuxinyu <lxy2019gm@gmail.com>
2019-06-01 17:57:08 +0800

    更新了98题的一种解法

--- LeetCode98.java
+++ LeetCode98.java
@@ -8,7 +8,7 @@ public class LeetCode98 {
       TreeNode right;
       TreeNode(int x) { val = x; }
  }
-    public static boolean isValidBST(TreeNode root) {
+    /*public static boolean isValidBST(TreeNode root) {
         List<TreeNode> list = new ArrayList<>();
         if(root==null) return true;
         orderBST(root,list);
@@ -23,6 +23,23 @@ public class LeetCode98 {
         orderBST(root.left,list);
         list.add(root);
         orderBST(root.right,list);
+    }*/
+
+    //左子树的最大值<根节点<右子树的最小值
+    public static boolean isValidBST(TreeNode root){
+        Integer min = Integer.MIN_VALUE;
+        Integer max = Integer.MAX_VALUE;
+        return validBST(root,min,max);
+    }
+    public static boolean validBST(TreeNode root,Integer min,Integer max){
+        if(root==null){
+            return true;
+        }else if(min>=root.val&&min!=null){
+            return false;
+        }else if(max<=root.val&&max!=null){
+            return false;
+        }
+        return validBST(root.left,min,root.val)&&validBST(root.right, root.val,max);
     }
     public static void main(String[] args) {
 
