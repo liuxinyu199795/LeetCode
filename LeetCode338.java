0022b8ccabce5c104621fcc989aa55aa0ca4444c
liuxinyu <lxy2019gm@gmail.com>
2021-03-03 21:20:13 +0800

    每日一题338题

--- LeetCode338.java
+++ LeetCode338.java
@@ -2,8 +2,9 @@
 public class LeetCode338 {
     public int[] countBits(int num) {
         int[] nums = new int[num+1];
-        for (int i = 1; i <num; i++) {
-            nums[i]=nums[i&(i-1)];
+        nums[0] = 0;
+        for (int i = 1; i <=num; i++) {
+            nums[i]=nums[i&(i-1)]+1;//i&(i-1)等于i去掉一个1,于是+1;
         }
         return nums;
     }
