7a856e0503e78802e800e01cab3239a19193d87a
liuxinyu <lxy2019gm@gmail.com>
2021-03-18 23:22:49 +0800

    每日一题92

--- LeetCode92.java
+++ LeetCode92.java
@@ -4,8 +4,8 @@ public class LeetCode92 {
         ListNode next;
         ListNode(int x) { val = x; }
     }
-
-    public ListNode reverseBetween(ListNode head, int m, int n) {
+    //1.普通法，翻转链表
+    /*public ListNode reverseBetween(ListNode head, int m, int n) {
         ListNode dummy = new ListNode(0);
         dummy.next=head;
         ListNode pre = dummy;
@@ -18,6 +18,23 @@ public class LeetCode92 {
             head.next=next.next;
             next.next=pre.next;
             pre.next=next;
+        }
+        return dummy.next;
+    }*/
+    //2.双指针，一个pre，将移动的指针的后一个插在pre前面
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next=head;
+        ListNode pre = dummy;
+        for (int i = 1; i <m ; i++) {
+            pre=pre.next;
+        }
+        head=pre.next;
+        for (int i = m; i <n ; i++) {
+            ListNode remove = head.next;
+            head.next = head.next.next;
+            remove.next=pre.next;
+            pre.next = remove;
         }
         return dummy.next;
     }
