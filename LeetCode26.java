1ab0c0e6c749f4b66cd577bf7bbf7a7206e7a26e
liuxinyu <lxy2019gm@gmail.com>
2021-04-18 15:38:27 +0800

    每日一题26

--- LeetCode26.java
+++ LeetCode26.java
@@ -2,16 +2,16 @@
 
 
 public class LeetCode26 {
+    //1.双指针法
     public static int removeDuplicates(int[] nums) {
-        if(nums.length == 0) return 0;
-        int i=0;
-        for(int j=1;j<nums.length;j++){
-            if(nums[j]!=nums[i]){
-                i++;
-                nums[i]=nums[j];
+        int n = nums.length;
+        int i = 0;
+        for (int j = 0; j < n; j++) {
+            if(nums[j] != nums[i]){
+                nums[++i] = nums[j];
             }
         }
-        return i;
+        return i+1;
     }
     public static void main(String[] args) {
         int[] nums=new int[]{1,2,3,2};
