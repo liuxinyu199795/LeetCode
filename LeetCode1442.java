91933180292333e4ba1f8db37a5daca00ba53c4f
liuxinyu <lxy2019gm@gmail.com>
2021-05-18 21:27:13 +0800

    每日一题1442

---
+++ LeetCode1442.java
@@ -0,0 +1,26 @@
+/**
+ * @ProjectName: LeetCode
+ * @Author: XinyuLiu
+ * @Description:
+ */
+public class LeetCode1442 {
+    //1.相同两个数的异或为0，所以求区间的异或值为0
+    public static int countTriplets(int[] arr) {
+        int res = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            int temp = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                temp ^= arr[j];
+                if(temp == 0){
+                    res += (j - i);
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] res = new int[]{2,3,1,6,7};
+        countTriplets(res);
+    }
+}
