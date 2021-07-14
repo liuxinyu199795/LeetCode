52511f8fae46904e4e6489c94620de362ecc0bbe
liuxinyu <lxy2019gm@gmail.com>
2021-07-14 21:18:36 +0800

    LeetCode1818

---
+++ LeetCode1818.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+/**
+ * @ProjectName: LeetCode
+ * @Author: XinyuLiu
+ * @Description:
+ */
+public class LeetCode1818 {
+    //1.排序后二分查找
+    int mod = (int)1e9+7;
+    public int minAbsoluteSumDiff(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int[] sorted = nums1.clone();
+        Arrays.sort(sorted);
+        long sum = 0, max = 0;
+        for (int i = 0; i < n; i++) {
+            int a = nums1[i], b = nums2[i];
+            if (a == b) continue;
+            int x = Math.abs(a - b);
+            sum += x;
+            int l = 0, r = n - 1;
+            while (l < r) {
+                int mid = l + r + 1 >> 1;
+                if (sorted[mid] <= b) l = mid;
+                else r = mid - 1;
+            }
+            int nd = Math.abs(sorted[r] - b);
+            if (r + 1 < n) nd = Math.min(nd, Math.abs(sorted[r + 1] - b));
+            if (nd < x) max = Math.max(max, x - nd);
+        }
+        return (int)((sum - max) % mod);
+    }
+}
