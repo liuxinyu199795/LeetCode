14f26bf193c2e746f67ebbb633540304b5bac1f9
347151535@qq.com <Lxy199795>
2021-10-02 22:50:19 +0800

    LeetCode405

---
+++ LeetCode405.java
@@ -0,0 +1,21 @@
+/**
+ * @ProjectName: LeetCode
+ * @Author: XinyuLiu
+ * @Description:
+ */
+public class LeetCode405 {
+    public String toHex(int _num) {
+        if (_num == 0) return "0";
+        long num = _num;
+        StringBuilder sb = new StringBuilder();
+        if(num < 0) num = (long)(Math.pow(2, 32) + num);
+        while (num != 0) {
+            long u = num % 16;
+            char c = (char)(u + '0');
+            if (u >= 10) c = (char)(u - 10 + 'a');
+            sb.append(c);
+            num /= 16;
+        }
+        return sb.reverse().toString();
+    }
+}
