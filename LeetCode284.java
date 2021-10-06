ac072350380d9e6b1c3211178b56af5451f3c213
liuxinyu199795 <lxy2019gm@gmail.com>
2021-10-06 19:32:28 +0800

    LeetCode284

--- LeetCode284.java
+++ LeetCode284.java
@@ -1,8 +1,29 @@
+import java.util.Iterator;
+
 /**
  * @ProjectName: LeetCode
  * @Author: XinyuLiu
  * @Description:
  */
 public class LeetCode284 {
+    Iterator<Integer> iter;
+    Integer next;
+    public LeetCode284(Iterator<Integer> iterator) {
+        iter = iterator;
+        if (iter.hasNext()) next = iter.next();
+    }
 
+    public Integer peek() {
+        return next;
+    }
+
+    public Integer next() {
+        Integer ans = next;
+        next = iter.hasNext() ? iter.next() : null;
+        return ans;
+    }
+
+    public boolean hasNext() {
+        return next != null;
+    }
 }
