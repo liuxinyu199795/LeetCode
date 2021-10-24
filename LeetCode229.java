6080c155d14333d1c2802bed186898cac47848bd
liuxinyu <lxy2019gm@gmail.com>
2021-10-24 23:54:39 +0800

    LeetCode229

---
+++ LeetCode229.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LeetCode229 {
+    //1.hash表计数
+    /*public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) map.put(i, map.getOrDefault(i, 0) + 1);
+        List<Integer> ans = new ArrayList<>();
+        for (int i : map.keySet()) {
+            if (map.get(i) > n / 3) ans.add(i);
+        }
+        return ans;
+    }*/
+    //2.摩尔投票
+    public List<Integer> majorityElement(int[] nums) {
+        // 创建返回值
+        List<Integer> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        // 初始化两个候选人candidate，和他们的计票
+        int cand1 = nums[0], count1 = 0;
+        int cand2 = nums[0], count2 = 0;
+
+        // 摩尔投票法，分为两个阶段：配对阶段和计数阶段
+        // 配对阶段
+        for (int num : nums) {
+            // 投票
+            if (cand1 == num) {
+                count1++;
+                continue;
+            }
+            if (cand2 == num) {
+                count2++;
+                continue;
+            }
+
+            // 第1个候选人配对
+            if (count1 == 0) {
+                cand1 = num;
+                count1++;
+                continue;
+            }
+            // 第2个候选人配对
+            if (count2 == 0) {
+                cand2 = num;
+                count2++;
+                continue;
+            }
+
+            count1--;
+            count2--;
+        }
+
+        // 计数阶段
+        // 找到了两个候选人之后，需要确定票数是否满足大于 N/3
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (cand1 == num) count1++;
+            else if (cand2 == num) count2++;
+        }
+
+        if (count1 > nums.length / 3) res.add(cand1);
+        if (count2 > nums.length / 3) res.add(cand2);
+
+        return res;
+    }
+}
