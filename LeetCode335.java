600cfe98d6d3c7a9ca52faa918cb7bba199eaf1d
liuxinyu <lxy2019gm@gmail.com>
2021-10-29 23:29:11 +0800

    LeetCode335

---
+++ LeetCode335.java
@@ -0,0 +1,12 @@
+public class LeetCode335 {
+    public boolean isSelfCrossing(int[] d) {
+        int n = d.length;
+        if (n < 4) return false;
+        for (int i = 3; i < n; i++) {
+            if (d[i] >= d[i - 2] && d[i - 1] <= d[i - 3]) return true;
+            if (i >= 4 && d[i - 1] == d[i - 3] && d[i] + d[i - 4] >= d[i - 2]) return true;
+            if (i >= 5 && d[i - 1] <= d[i - 3] && d[i - 2] > d[i - 4] && d[i] + d[i - 4] >= d[i - 2] && d[i - 1] + d[i - 5] >= d[i - 3]) return true;
+        }
+        return false;
+    }
+}
