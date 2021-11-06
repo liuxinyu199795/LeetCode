f548738a7ff75ad04cfbb7abcf650d81f5bf3b12
liuxinyu <lxy2019gm@gmail.com>
2021-11-06 12:45:35 +0800

    LeetCode1218

--- LeetCode1218.java
+++ LeetCode1218.java
@@ -17,38 +17,4 @@ public class LeetCode1218 {
         }
         return Math.max(dp[length - 1][0],dp[length - 1][1]);
     }
-
-    public static void main(String[] args) {
-        /*String s1 = new String("abc");//语句1
-        String s2 = "abc";//语句2
-        String s3 = new String("abc");//语句3
-
-        System.out.println(s1 == s2);//语句4
-        System.out.println(s1 == s3);//语句5
-        System.out.println(s2 == s3);//语句6
-
-        System.out.println(s1 == s1.intern());//语句7
-        System.out.println(s2 == s2.intern());//语句8
-        System.out.println(s1.intern() == s2.intern());//语句9
-
-        String hello = "hello";//语句10
-        String hel = "hel";//语句11
-        String lo = "lo";//语句12
-
-        System.out.println(hello == "hello");//语句13
-        System.out.println(hello == "hel" + "lo");//语句14
-        System.out.println(hello == "hel" + lo);//语句15
-        System.out.println(hello == hel + lo);//语句16
-
-        String s = new String("hello");
-        System.out.println(s.intern()==s);*/
-
-        String sb = new StringBuilder().append("hel").append("lo").toString();
-        System.out.println(sb.intern() == sb);
-        String ss = new String("hel");
-        System.out.println(ss == ss.intern());
-
-        String sb1 = new StringBuilder().append("lx").append("y").toString();
-        System.out.println(sb1.intern() == sb1);
-    }
 }
