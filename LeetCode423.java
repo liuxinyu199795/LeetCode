3f99d38e3056de7653a9499d2218d4daa812cc90
liuxinyu <lxy2019gm@gmail.com>
2021-11-24 21:39:36 +0800

    LeetCode423

---
+++ LeetCode423.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class LeetCode423 {
+    static String[] ss = new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    static int[] priority = new int[]{0, 8, 6, 3, 2, 7, 5, 9, 4, 1};
+    public String originalDigits(String s) {
+        int n = s.length();
+        int[] cnts = new int[26];
+        for (int i = 0; i < n; i++) cnts[s.charAt(i) - 'a']++;
+        StringBuilder sb = new StringBuilder();
+        for (int i : priority) {
+            int k = Integer.MAX_VALUE;
+            for (char c : ss[i].toCharArray()) k = Math.min(k, cnts[c - 'a']);
+            for (char c : ss[i].toCharArray()) cnts[c - 'a'] -= k;
+            while (k-- > 0) sb.append(i);
+        }
+        char[] cs = sb.toString().toCharArray();
+        Arrays.sort(cs);
+        return String.valueOf(cs);
+    }
+}
