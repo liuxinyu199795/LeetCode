08454715768d50d77a0f5b4a96a495211a43ac99
liuxinyu199795 <lxy2019gm@gmail.com>
2022-01-03 20:28:48 +0800

    LeetCode1185

---
+++ LeetCode1185.java
@@ -0,0 +1,22 @@
+/**
+ * @ProjectName: LeetCode
+ * @Author: XinyuLiu
+ * @Description:
+ */
+public class LeetCode1185 {
+    static String[] ss = new String[]{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    static int[] nums = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public String dayOfTheWeek(int day, int month, int year) {
+        int ans = 4;
+        for (int i = 1971; i < year; i++) {
+            boolean isLeap = (i % 4 == 0 && i % 100 != 0) || i % 400 == 0;
+            ans += isLeap ? 366 : 365;
+        }
+        for (int i = 1; i < month; i++) {
+            ans += nums[i - 1];
+            if (i == 2 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) ans++;
+        }
+        ans += day;
+        return ss[ans % 7];
+    }
+}
