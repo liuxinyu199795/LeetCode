3223444aba765136d5260263893076e8160d010f
liuxinyu <lxy2019gm@gmail.com>
2022-01-09 21:00:31 +0800

    Jianz005

---
+++ Jianz005.java
@@ -0,0 +1,13 @@
+public class Jianz005 {
+    public String replaceSpace (String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if(c == ' '){
+                sb.append("%20");
+            }else{
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
