9d56b1411108e330b5d445f2b8feab41d30dff3c
liuxinyu <lxy2019gm@gmail.com>
2022-01-20 21:50:02 +0800

    LeetCode2029

---
+++ LeetCode2029.java
@@ -0,0 +1,7 @@
+public class LeetCode2029 {
+    public boolean stoneGameIX(int[] stones) {
+        int[] cnts = new int[3];
+        for (int i : stones) cnts[i % 3]++;
+        return cnts[0] % 2 == 0 ? !(cnts[1] == 0 || cnts[2] == 0) : !(Math.abs(cnts[1] - cnts[2]) <= 2);
+    }
+}
