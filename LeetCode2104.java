a110c259870ba276c4edd141a606236d21fe0d4e
liuxinyu <lxy2019gm@gmail.com>
2022-03-05 17:00:24 +0800

    LeetCode2104

---
+++ LeetCode2104.java
@@ -0,0 +1,25 @@
+public class LeetCode2104 {
+    //1.序列DP
+    public long subArrayRanges(int[] nums) {
+        int n = nums.length;
+        int[][][] dp = new int[n][n][2];
+        //初始化数组
+        for (int i = 0; i < n; i++) {
+            dp[i][i][0] = dp[i][i][1] = nums[i];
+        }
+        for (int len = 2; len <= n; len++) {
+            for (int l = 0;  l + len - 1 < n; l++) {
+                int r = l + len - 1;
+                dp[l][r][0] = Math.min(dp[l][r - 1][0],nums[r]);
+                dp[l][r][1] = Math.max(dp[l][r - 1][1],nums[r]);
+            }
+        }
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                res += dp[i][j][1] - dp[i][j][0];
+            }
+        }
+        return res;
+    }
+}
