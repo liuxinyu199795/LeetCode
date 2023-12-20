6e1d6a02b652fadb7cda1522dc7bfb52ea7afe8c
liuxinyu <lxy2019gm@gmail.com>
2023-12-20 19:56:20 +0800

    LeetCode1

---
+++ LeetCode1.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ProjectName: LeetCode
+ * @Author: XinyuLiu
+ * @Description:
+ */
+public class LeetCode1 {
+    /*public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return null;
+    }*/
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i])){
+                return new int[]{map.get(target - nums[i]),i};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+}
