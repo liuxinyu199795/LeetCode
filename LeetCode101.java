3340a185f2e7fe7fef2489a1e2da1b621ae47986
liuxinyu <lxy2019gm@gmail.com>
2024-01-02 23:53:25 +0800

    LeetCode101

---
+++ LeetCode101.java
@@ -0,0 +1,27 @@
+public class LeetCode101 {
+    public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+   }
+    public boolean isSymmetric(TreeNode root) {
+        return recur(root,root);
+    }
+    private boolean recur(TreeNode p,TreeNode q){
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        return p.val == q.val && recur(p.left,q.right) && recur(p.right,q.left);
+    }
+
+}
