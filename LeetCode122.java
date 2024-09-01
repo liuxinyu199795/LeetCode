99ce767d8c22990e94f1e14a7e38dc9b8243f337
liuxinyu <lxy2019gm@gmail.com>
2024-09-01 23:08:06 +0800

    LeetCode122更新算法

--- LeetCode122.java
+++ LeetCode122.java
@@ -11,18 +11,16 @@ public class LeetCode122 {
     }*/
     //2.动态规划
     public int maxProfit(int[] prices) {
-        if(prices==null||prices.length==0) return 0;
-        int length =prices.length;
+        int length = prices.length;
+        if(length < 2) return 0;
         int[][] dp = new int[length][2];
-        dp[0][0]=0;
-        dp[0][1]=-prices[0];
-        int res=0;
-        for (int i = 1; i <length ; i++) {
-            dp[i][0]=Math.max(dp[i-1][0],dp[i-1][1]+prices[i]);
-            dp[i][1]=Math.max(dp[i-1][1],dp[i-1][0]-prices[i]);
-            res=Math.max(res,Math.max(dp[i][0],dp[i][1]));
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < length; i++) {
+            dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1] + prices[i]);//持有现金，今天卖出股票
+            dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0] - prices[i]);//持有股票，今天买入股票
         }
-        return res;
+        return dp[length - 1][0];
     }
     public static void main(String[] args) {
     }
